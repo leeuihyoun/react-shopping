b3c3eca8479f506d89ac1033d6c642c39f38f63b
vanillwa <yjh30448813@gmail.com>
2024-05-12 09:49:24 +0900

    2024-05-12

--- src/components/cart-tile/cart-tile.jsx
+++ src/components/cart-tile/cart-tile.jsx
@@ -1,7 +1,26 @@
-
+import { useDispatch } from 'react-redux'
+import './cart-tile.css'
+import { removeFromCart } from '../../store/slices/cart-slice';
 
 export default function CartTile({cartItem}){
+	const dispatch = useDispatch();
+	function hRemoveFromCart(){
+		dispatch(removeFromCart(cartItem.id));
+	}
+		//리덕스의 함수를 사용하기 위해선 useDispatch()
+		//리덕스(store)의 데이터를 사용하기 위해선 useSelector()
 	return(
-		<div>카트 타일</div>
+		<div className='tile-container'>
+			<div className='flex-padding'>
+				 <img src={cartItem?.image} alt={cartItem?.title} className='rounded-box'/>
+				<div className='title-content'>
+					<h1 >{cartItem.title}</h1>
+					<p >{Math.floor(cartItem.price * 1300).toLocaleString('ko-KR')}원</p>
+				</div>
+			</div>
+			<div>
+				<button onClick={hRemoveFromCart} className = 'bold-red-btn'>제거</button>
+			</div>
+		</div>
 	)
 }
